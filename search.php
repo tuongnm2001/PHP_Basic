6f422671a364121b00c1b2e7a0f92016fbfab083
Tuong Nguyen <tuongnm2001@gmail.com>
2023-05-18 10:39:14 +0700

    search

---
+++ search.php
@@ -0,0 +1,18 @@
+<?php 
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $name = $_POST['name'];
+        if($name=='tuong'){
+            echo 'Nguyen Minh Tuong';
+        }else if($name=='trent'){
+            echo 'Trent Alexander-Arnold';
+        }else{
+            echo 'Empty';
+        }
+
+    }
+?>
+
+<form action='search.php' method="POST">
+    Name : <input type='text' name ='name'/>
+    <input type='submit'/>
+</form>
