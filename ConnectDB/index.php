7ee647c58d42c496d0dcebcca125e4e77eacefa8
Tuong Nguyen <tuongnm2001@gmail.com>
2023-05-19 10:34:43 +0700

    read user

--- ConnectDB/index.php
+++ ConnectDB/index.php
@@ -1,20 +1,35 @@
 <?php 
     include 'connect.php' ;
 
-    $firstName = 'Joe';
-    $lastName = 'Gomez';
-    $email = 'jg2@mail.com';
-    $emailVerifiedAt = 'weknngkwel';
-    $password = '263526592';
-    $rememberToken = 'wegujwefj';
+    //CREARE USER
 
-    $query = "INSERT INTO  `users` (`first_name` , `last_name`, `email`,`email_verified_at`, `password`, `remember_token` )
-        VALUES ('$firstName',
-        '$lastName',
-        '$email',
-        '$emailVerifiedAt',
-        '$password',
-        '$rememberToken')";
-    $result = mysqli_query($conn, $query);
+    // $firstName = 'Joe';
+    // $lastName = 'Gomez';
+    // $email = 'jg2@mail.com';
+    // $emailVerifiedAt = 'weknngkwel';
+    // $password = '263526592';
+    // $rememberToken = 'wegujwefj';
 
+    // $query = "INSERT INTO  `users` (`first_name` , `last_name`, `email`,`email_verified_at`, `password`, `remember_token` )
+    //     VALUES ('$firstName',
+    //     '$lastName',
+    //     '$email',
+    //     '$emailVerifiedAt',
+    //     '$password',
+    //     '$rememberToken')";
+    // $result = mysqli_query($conn, $query);
+
+
+    //READ USER
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($conn , $sql);
+
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_array($result)){
+            echo $row['id'] . " |  " . $row['first_name'] . "  | " . $row['last_name'] . " |  " . $row['email'] . "  | " . $row['email_verified_at'];
+            echo "<br/>" ;
+        }
+        
+    }
+   
 ?>
