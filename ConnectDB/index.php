821e898a743cd017ccdb4a0fb2f96ce19d9665bc
Tuong Nguyen <tuongnm2001@gmail.com>
2023-05-19 11:09:18 +0700

    delete user

--- ConnectDB/index.php
+++ ConnectDB/index.php
@@ -21,15 +21,21 @@
 
 
     //READ USER
-    $sql = "SELECT * FROM users";
-    $result = mysqli_query($conn , $sql);
+    // $sql = "SELECT * FROM users";
+    // $result = mysqli_query($conn , $sql);
 
-    if(mysqli_num_rows($result) > 0){
-        while($row = mysqli_fetch_array($result)){
-            echo $row['id'] . " |  " . $row['first_name'] . "  | " . $row['last_name'] . " |  " . $row['email'] . "  | " . $row['email_verified_at'];
-            echo "<br/>" ;
-        }
+    
+    // if(mysqli_num_rows($result) > 0){
+    //     while($row = mysqli_fetch_array($result)){
+    //         echo $row['id'] . " |  " . $row['first_name'] . "  | " 
+    //         . $row['last_name'] . " |  " . $row['email'] . "  | " . $row['email_verified_at'] . "" ;
+    //         echo "<br/>" ;
+    //     }
         
-    }
+    // }
+
+    //DETELE USER 
+    $sql = "DELETE FROM `users` WHERE id='26' ";
+    mysqLi_query($conn , $sql);
    
 ?>
